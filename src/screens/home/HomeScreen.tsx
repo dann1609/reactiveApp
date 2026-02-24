8e59a02cfef702a8af569fb407eedb414ea7b503
Daniel Padilla <dpadilla1005@gmail.com>
2026-02-23 22:22:44 -0800

    Api call added to app.

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,7 +1,14 @@
 import { Text } from "react-native";
 import { Layout } from "../../components";
+import { useEffect } from "react";
+import { getProductsFromApi } from "../../services/products";
 
 export default function HomeScreen() {
+
+    useEffect(() => {
+        getProductsFromApi();
+    }, []);
+
     return (
         <Layout
             disableBottomInset
---
+++ src/services/products.ts
@@ -0,0 +1,11 @@
+export const getProductsFromApi = () => {
+    return fetch('https://gist.githubusercontent.com/agorovyi/40dcd166a38b4d1e9156ad66c87111b7/raw/36f1c815dd83ed8189e55e6e6619b5d7c7c4e7d6/testProducts.json')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            return json
+        })
+        .catch(error => {
+            console.error(error);
+        });
+};
